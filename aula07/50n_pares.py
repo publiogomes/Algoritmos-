65a7fd5941d3adf317819fe5282e1917a6398e7f
Publio Gomes <publiogomes@hotmail.com>
2023-05-17 19:24:59 -0300

    Delete aula07 directory

--- aula07/50n_pares.py
+++
@@ -1,8 +0,0 @@
-soma = 0
-par = 2
-for i in range(50):
-    soma = soma + par
-    par = par + 2
-    print(f"A soma dos 50 primeiros numero pares é {soma}")
-
-
--- aula07/valor_e.py
+++
@@ -1,6 +0,0 @@
-import math
-e = 0
-n = int(input("Digite um número inteiro positivo: "))
-for i in range(1, n+1):
-    e = e + math.pow(2, i) # você pode colocar tbm e = e + 2**1, e não importaria math
-    print((f"O valor de E = {e:.2f}"))
